2375d9ff089dbbacee7c7ea8dc19f0861180ae7f
ILIA PETROV <ilia-p@yandex.ru>
2017-07-12 17:26:04 +0300

    Модификация по комментариям от 12052017 от Ильи Лебедева

--- bars.py
+++ bars.py
@@ -7,6 +7,21 @@ from math import sqrt
 parser = argparse.ArgumentParser(description = 'Path_to_file')
 parser.add_argument('-p', '--path', type = str, required = True, help = 'Path to file with bar data in json')
 
+def check_path(arg):
+    file_path = ''
+    while True:
+        if os.path.exists(arg.path):
+            filepath = arg.path
+            return filepath
+            break
+        elif os.path.exists(file_path):
+            filepath = file_path
+            return filepath
+            break
+        else:
+            file_path = input('Такого пути/файла не существует, повторите ввод:\n')
+            continue
+
 def load_data(filepath):
     with open(filepath) as total_bar_list:
         bar_data = json.load(total_bar_list)
@@ -21,6 +36,16 @@ def get_biggest_bars(bar_data):
     max_seat_bar_dict = max(bar_data, key=lambda x:x['SeatsCount'])
     max_seat_bar = max_seat_bar_dict['Name']
     return max_seat_bar
+
+def check_coordinate(coordinate):
+    while True:
+        if re.compile(r'\d{2}\.\d*').search(coordinate):
+            coordinate = float(coordinate)
+            return coordinate
+            break
+        else:
+            coordinate = input('Введено некорректное значение, повторите ввод:\n')
+            continue
 
 def get_closest_bars(bar_data, longitude, latitude):
     bar_dist_dict = {}
@@ -39,35 +64,13 @@ def print_target_bars(min_seat_bar, max_seat_bar, bar_min_dist):
 if __name__ == '__main__':
     
     arg = parser.parse_args()
-    filepath = ''
-    while True:
-        if os.path.exists(arg.path):
-            bar_data = load_data(arg.path)
-            break
-        elif os.path.exists(filepath):
-            bar_data = load_data(filepath)
-            break
-        else:
-            filepath = input('Такого пути/файла не существует, повторите ввод:\n')
-            continue
+    filepath = check_path(arg)
+    bar_data = load_data(filepath)
     min_seat_bar = get_smallest_bars(bar_data)
     max_seat_bar = get_biggest_bars(bar_data)
-    longitude = input('\nВведите долготу: ')
-    while True:
-        if re.compile(r'\d{2}\.\d*').search(longitude):
-            longitude= float(longitude)
-            break
-        else:
-            longitude = input('Введено некорректное значение, повторите ввод:\n')
-            continue
-    latitude  = input('Введите широту: ')
-    while True:
-        if re.compile(r'\d{2}\.\d*').search(latitude):
-            latitude= float(latitude)
-            break
-        else:
-            latitude = input('Введено некорректное значение, повторите ввод:\n')
-            continue
+    longitude_initial = input('\nВведите долготу: ')
+    longitude = check_coordinate(longitude_initial)
+    latitude_initial  = input('Введите широту: ')
+    latitude = check_coordinate(latitude_initial)
     bar_min_dist = get_closest_bars(bar_data, longitude, latitude)
     print_target_bars(min_seat_bar, max_seat_bar, bar_min_dist)
-        
